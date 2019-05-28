7c3771ce59481b1291deb9f7043b0072fa0236c4
liuyunfeng2011 <liuyunfeng2011@gmail.com>
2019-05-28 09:59:39 +0800

    添加了User类用来管理用户 使用ajax完成用户登录

--- src/com/gezhi/controller/LoginServlet.java
+++ src/com/gezhi/controller/LoginServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,62 +15,63 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet{
+import com.gezhi.pojo.User;
 
-
+public class LoginServlet extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//接受前端请求
-		String name=req.getParameter("uName");//""
-		String pwd=req.getParameter("uPwd");
-//		//jdbc
-//		String dbName="zhangsan";
-//		String dbPwd="admin";
-//		if(name.equals(dbName)&&pwd.equals(dbPwd)){
-//			System.out.println("登录成功");
-//			resp.sendRedirect("main.html");//重定向到某一个资源
-//		}else{
-//			System.out.println("用户名/密码错误");
-//			resp.sendRedirect("www.baidu.com");//重定向到某一个资源
-//		}
-		//1:连接数据库
-				//获得驱动
-		Connection conn=null;
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=resp.getWriter();
+		// 接受前端请求
+		String name = req.getParameter("uName");// ""
+		String pwd = req.getParameter("uPwd");
+		// 1:连接数据库
+		// 获得驱动
+		Connection conn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://127.0.0.1:3306/tb40";
-			 conn= DriverManager.getConnection(url,"root","admin");
-		//sql语句的操作
-			String sql="select user_id,user_name,user_pwd,user_type from users where user_name='zhangsan'";
-			//获取
-			Statement stmt=conn.createStatement();
-			//执行sql
-			ResultSet rs=stmt.executeQuery(sql);
-			//操作
+			conn = DriverManager.getConnection(url, "root", "admin");
+			String sql = "select user_id,user_name,user_type from users where user_name=? and user_pwd=?";
+			PreparedStatement ptmt = conn.prepareStatement(sql);
+			ptmt.setString(1, name);
+			ptmt.setString(2, pwd);
+			ResultSet rs= ptmt.executeQuery();
+			User user=null;
 			while(rs.next()){
-				int uId=rs.getInt("user_id");
-				String uName=rs.getString("user_name");
-				String uPwd=rs.getString("user_pwd");
-				String uType=rs.getString("user_type");
-				System.out.println(uId+" "+uName+" "+uPwd+" "+uType);
+				int userId=rs.getInt("user_id");
+				String userName=rs.getString("user_name");
+				int userType=rs.getInt("user_type");
+				user=new User(userId,userName,userType);
+			}
+			if(user!=null){
+				if(user.getUserType()==0){
+					//禁止登录
+					out.print("0");
+				}else{
+					//登录成功
+					out.print("1");
+				}
+			}else{
+				//帐户名/密码错误
+				out.print("3");
 			}
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
 			System.out.println("找不到驱动|sql有异常");
-		}finally {
-			//关闭数据库
-			if(conn!=null){
+		} finally {
+			// 关闭数据库
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-					//记录就可以
+					// 记录就可以
 				}
 			}
 		}
-		
+
 	}
 
 	@Override
@@ -78,5 +80,4 @@ public class LoginServlet extends HttpServlet{
 		System.out.println("xiaohui le");//
 	}
 
-	
 }
---
+++ src/com/gezhi/pojo/User.java
@@ -0,0 +1,67 @@
+package com.gezhi.pojo;
+
+import java.io.Serializable;
+/**
+ * 用户类
+ * @author Liuyf
+ *
+ */
+public class User implements Serializable{
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(String userName, String userPwd, Integer userType) {
+		super();
+		this.userName = userName;
+		this.userPwd = userPwd;
+		this.userType = userType;
+	}
+	
+	public User(Integer userId, String userName, Integer userType) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userType = userType;
+	}
+
+	//用户ID
+	private Integer userId;
+	//用户姓名
+	private String userName;
+	//用户密码
+	private String userPwd;
+	//用户状态 1-正常  0-禁止登录
+	private Integer userType;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public Integer getUserType() {
+		return userType;
+	}
+	public void setUserType(Integer userType) {
+		this.userType = userType;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userPwd=" + userPwd + ", userType=" + userType
+				+ "]";
+	}
+	
+}
